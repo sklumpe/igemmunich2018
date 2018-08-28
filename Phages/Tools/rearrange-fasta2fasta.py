100fd22db7cd9dfa4447cec6c988e9bc8508b2b9
Julia Mayer <juliamayer@me.com>
2018-08-28 14:13:29 +0200

    Debugging Rearrangement Skript
    False variable naming was corrected.

--- Phages/Tools/rearrange-fasta2fasta.py
+++ Phages/Tools/rearrange-fasta2fasta.py
@@ -5,7 +5,7 @@ from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
-#rearrange one contig according to different indices
+# rearrange one contig according to different indices
 
 parser = argparse.ArgumentParser()
 # --fasta input, --o output, --id to be manipulated, --i indices
@@ -23,17 +23,20 @@ with open(args.o[0], 'w') as fasta_outfile:
         if record.id in args.id:
             length = len(record.seq)
 
+            original_record = record
             sequences = []
+            count = 0
 
             for i in args.i:
-                sequence_new = str(record.seq[i:length]) + str(record.seq[0:i])
+                sequence_new = str(original_record.seq[i:length]) + str(original_record.seq[0:i])
 
                 record = SeqRecord(Seq(sequence_new),
-                                   id=str(record.id),
+                                   id=str(original_record.id) + '_' + str(count),
                                    description='indices: ('
                                                + str(i) + ',' + str(length - 1) + ')'
                                                + ' + (' + str(0) + ',' + str(i - 1) + ')')
 
                 sequences.append(record)
+                count += 1
 
     SeqIO.write(sequences, fasta_outfile, 'fasta')
